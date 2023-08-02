dde695ffddb9b05da8c9b057e46a2b755bb3a88c
mirsaalf <mirsaalf@gmail.com>
2023-08-02 12:58:59 -0700

    Configured DbContext

--- eCommerceSite/Program.cs
+++ eCommerceSite/Program.cs
@@ -1,8 +1,14 @@
+using eCommerceSite.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SkinCareContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
