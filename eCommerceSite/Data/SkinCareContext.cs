9b012b6ba36f2007c92bd0f000a92a42c9a7d914
mirsaalf <mirsaalf@gmail.com>
2023-08-02 12:46:26 -0700

    Added DbContext class

---
+++ eCommerceSite/Data/SkinCareContext.cs
@@ -0,0 +1,15 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceSite.Data
+{
+    public class SkinCareContext : DbContext
+    {
+        public SkinCareContext(DbContextOptions<SkinCareContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Skin> Skins { get; set; }
+    }
+}
